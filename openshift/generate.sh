4bd7618cd96e906bbbc96e79d11dab7e8c16afc3
Christoph Stäbler <cstabler@redhat.com>
2023-03-28 12:59:50 +0200

    Use GOPATH env when calling hack/cmd/generate (#167)

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -6,7 +6,7 @@ repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
 GO111MODULE=off go get -u github.com/openshift-knative/hack/cmd/generate
 
-generate \
+$(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile
 
