e8eb1843a421996968e3062760f7e700baddd9c9
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-01-23 16:37:25 +0100

    Generate dockerfiles using the a different tag
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -8,4 +8,5 @@ GO111MODULE=off go get -u github.com/openshift-knative/hack/cmd/generate
 
 $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
-  --generators dockerfile
+  --generators dockerfile \
+  --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.16"
