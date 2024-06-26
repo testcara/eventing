bc894043992edc8b3ae778b2eda12cd3583964f7
Christoph Stäbler <cstabler@redhat.com>
2024-06-26 12:37:30 +0200

    Enable debug logs in generate.sh (#647)

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -xeuo pipefail
 
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
@@ -12,3 +12,5 @@ $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile \
   --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.16"
+
+set +x
