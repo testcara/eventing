e61c3bde8f23ee6d5f7a98efd09c9768611ca736
Christoph Stäbler <cstabler@redhat.com>
2024-06-26 11:33:39 +0200

    Switch to go install to install hack/generate (#645)
    * Switch to go install to install hack/generate
    
    * Make sure to install latest hack/generate

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
-GO111MODULE=off go get -u github.com/openshift-knative/hack/cmd/generate
+# make sure we install the latest version
+rm -f $(go env GOPATH)/bin/generate
+GO111MODULE=off go install github.com/openshift-knative/hack/cmd/generate
 
 $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
