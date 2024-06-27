9f79a50ee4e03859f569b9bbfbbc7cf2780b70c2
Christoph Stäbler <cstabler@redhat.com>
2024-06-27 10:37:08 +0200

    Cleanup after hack repo clone (#655)
    * Revert "Enable debug logs in generate.sh (#647)"
    
    This reverts commit bc894043992edc8b3ae778b2eda12cd3583964f7.
    
    * Cleanup after hack repo generate install

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
@@ -11,9 +11,9 @@ pushd "$tmp_dir"
 go install github.com/openshift-knative/hack/cmd/generate
 popd
 
+rm -rf "$tmp_dir"
+
 $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile \
   --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.16"
-
-set +x
