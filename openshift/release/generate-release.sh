916e26b835a2330472889cc2a61d23838e7a66cf
Martin Gencur <mgencur@redhat.com>
2024-06-28 16:07:24 +0200

    Workaround for OCPBUGS-35731 Repeat deleting service accounts (#652)
    * Repeat deleting service accounts in reconciler-tests
    
    * Patch for deleting service accounts
    
    * List the patch in generate-release.sh

--- openshift/release/generate-release.sh
+++ openshift/release/generate-release.sh
@@ -14,6 +14,7 @@ echo "Release: $release"
 "${root_dir}"/hack/update-codegen.sh
 git apply "${root_dir}"/openshift/patches/018-rekt-test-override-kopublish.patch
 git apply "${root_dir}"/openshift/patches/020-mutemetrics.patch
+git apply "${root_dir}"/openshift/patches/027-rekt-serviceaccounts-delete.patch
 
 ./openshift/generate.sh
 
